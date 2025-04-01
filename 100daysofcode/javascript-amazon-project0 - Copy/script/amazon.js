9fe3775c2567bcea365ebe9aa8c26c4069c88986
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-04-01 20:00:12 +0530

    Use product.js

--- 100daysofcode/javascript-amazon-project0 - Copy/script/amazon.js
+++ 100daysofcode/javascript-amazon-project0 - Copy/script/amazon.js
@@ -1,14 +1,4 @@
-const products  = [{
-  image :' images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: ' Black and Gray Athletic Cotton Socks - 6 Pairs',
 
-  rating: {
-     stars : 4.5,
-     count: 87
-  },
-   priceCents:1090
-
-}];
 
  let productHTML  = '';
 products.forEach((product) => {
