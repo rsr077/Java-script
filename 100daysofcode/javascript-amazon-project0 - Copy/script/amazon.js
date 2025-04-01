5d213b1e0b61417c7d8a8d387520e102b5821b67
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-04-01 19:55:56 +0530

    hI

--- 100daysofcode/javascript-amazon-project0 - Copy/script/amazon.js
+++ 100daysofcode/javascript-amazon-project0 - Copy/script/amazon.js
@@ -1,32 +1,41 @@
-import {cart, addToCart} from '../data/cart.js'
-import { products } from '../data/products.js'
+const products  = [{
+  image :' images/products/athletic-cotton-socks-6-pairs.jpg',
+  name: ' Black and Gray Athletic Cotton Socks - 6 Pairs',
 
+  rating: {
+     stars : 4.5,
+     count: 87
+  },
+   priceCents:1090
 
-    let productsHTML  = ''
+}];
+
+ let productHTML  = '';
 products.forEach((product) => {
-    productsHTML  += 
-   
-    `      <div class="product-container">
+
+        productHTML += ` 
+    
+    
+    <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            ${product.name}
+               ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars * 10}.png">
-
             <div class="product-rating-count link-primary">
-         ${product.rating.count}
+             ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-          $${(product.priceCents/100).toFixed(2)}
+             $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -51,36 +60,11 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${product.id}">
+          <button class="add-to-cart-button button-primary">
             Add to Cart
           </button>
-        </div>`
-})
-
-
-    document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
-   
-
-     function updateCartQuantity() {
-      let cartQuantity = 0
-      cart.forEach((cartItem) => {
-        cartQuantity  += cartItem.quantity;
-      })
-       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-     }
-
-
-
-    document.querySelectorAll('.js-add-to-cart')
-    .forEach((button) => {
-      button.addEventListener('click', () => {
-         const productId  = button.dataset.productId;
-
-          addToCart( productId);
-          updateCartQuantity();
-        
-       
-   });
-   console.log(updateCartQuantity)
+        </div>
+        `
 });
+
+document.querySelector('.js-product-grid').innerHTML = productHTML;
