ce965ce386dd41092ebd1bd458f0762a66a0a5ee
Rajeev Singh Rajput <yourfriend77777777@gmail.com>
2025-04-03 11:53:09 +0530

    jasmine

--- 100daysofcode/javascript-amazon-project0 - Cop/scripts/amazon.js
+++ 100daysofcode/javascript-amazon-project0 - Cop/scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product)=> {
           </div>
 
           <div class="product-price">
-             ${formatCurrency(product.priceCents)}
+             $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
---
+++ 100daysofcode/javascript-amazon-project0 - Cop/tests/moneryTest.js
@@ -0,0 +1,16 @@
+import {formatCurrency} from '../scripts/utils/monery.js'
+
+  console.log('converts cents into dolloars');
+if( formatCurrency(2095) === '20.95') {
+  console.log('passed')
+}else {
+   console.log('failed');
+}
+
+ console.log('works with 0')
+
+if(formatCurrency(0) === '0.00') {
+   console.log('passed');
+}else {
+   console.log('failed')
+}
